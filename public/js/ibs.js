1d89e2003ff9caf759fedfdfe7d4b30d75e08142
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-06-10 18:00:26 -0700

    Add ability to update database once online

--- public/js/ibs.js
+++ public/js/ibs.js
@@ -7,4 +7,54 @@ request.onupgradeneeded = function(event){
 }
 request.onsuccess = function (event) {
     db = event.target.result;
+    if(navigator.onLine){
+        updateDatabase();
+    }
 }
+
+request.onerror = function(event) {
+    // log error here
+    console.log("Seems an error occured!" + event.target.errorCode);
+  };
+
+function saveRecord(record){
+    const transaction = db.transaction(["pending"],"readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
+function updateDatabase () {
+    const transaction = db.transaction(["pending"],"readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function (){
+        if (getAll.result.length > 0) {
+            fetch ("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+              // if successful, open a transaction on your pending db
+              const transaction = db.transaction(["pending"], "readwrite");
+    
+              // access your pending object store
+              const store = transaction.objectStore("pending");
+    
+              // clear all items in your store
+              store.clear();
+            });
+        }
+
+    }
+}
+
+window.addEventListener("online", updateDatabase);
